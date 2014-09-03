2f7ce65d3df8d6afb000807e2e5d1915d23d9baf
dc1394 <hriver2014-webservice10@memoad.jp>
2014-09-03 16:57:40 +0900

    ひたすらコメントを書く＆仕様変更

--- Thomas_Fermi/MakeRhoEn/MakeRhoEnergy.cpp
+++ Thomas_Fermi/MakeRhoEn/MakeRhoEnergy.cpp
@@ -17,50 +17,41 @@ namespace Thomas_Fermi {
 
 		double MakeRhoEnergy::rhofunc(double x) const
 		{
-#ifdef __GXX_EXPERIMENTAL_CXX0X__
-			static constexpr double fixednum = 32.0 / (9.0 * power(boost::math::constants::pi<double>(), 3));
-#else
 			static const double fixednum = 32.0 / (9.0 * power(boost::math::constants::pi<double>(), 3));
-#endif			
+			
 			return fixednum * (y(x) / x) * std::sqrt(y(x) / x);
 		}
 
 		double MakeRhoEnergy::makeEnergy() const
 		{
-			return 3.0 / 7.0 * ALPHA * v1_;
+			//return 3.0 / 7.0 * ALPHA * v1_;
 		}
 
-		void MakeRhoEnergy::saveresult1()
+        void MakeRhoEnergy::savey(const std::string & filename)
 		{
-			ofs.open("result1.csv");
+			ofs_.open(filename);
 
 			for (std::uint32_t i = 0; i < size_; i++)
-				ofs << xvec_[i] << boost::format(",%.15f\n") % y(xvec_[i]);
+				ofs_ << xvec_[i] << ' ' << y(xvec_[i]) << '\n';
 
-			ofs.close();
+			ofs_.close();
 		}
 
-		void MakeRhoEnergy::saveresult2(std::size_t n, bool useAVXorSSE, bool usecilk)
+		void MakeRhoEnergy::saverho(const std::string & filename)
 		{
-			ofs.open("result2.csv");
-
-			FEM_ALL::Gauss_Legendre gl(n, usecilk);
-			a = gl.qgauss(xvec_[0], xvec_[size_ - 1], useAVXorSSE,
-					      [&](double x) { return x * x * rhofunc(x); }) / 
-							power(MakeRhoEnergy::ALPHA, 3);
+			ofs_.open(filename);
 
 			for (std::int32_t i = 1; i <= max_; i++) {
 				const double r = static_cast<double>(i) * dx_;
-				ofs << r << boost::format(",%.15f,%.15f\n") % (rhofunc(MakeRhoEnergy::ALPHA * r) / a)
-															% (4.0 * std::exp(- 2.0 * r));
+				ofs_ << r << boost::format(",%.15f,%.15f\n") % (rhofunc(MakeRhoEnergy::ALPHA * r) / a);
 			}
 
-			ofs.close();
+			ofs_.close();
 		}
 
 		void MakeRhoEnergy::saveresult3()
 		{
-			ofs.open("result3.csv");
+			ofs_.open("result3.csv");
 
 			for (std::int32_t i = 1; i <= max_; i++) {
 				const double r = static_cast<double>(i) * dx_;
@@ -68,18 +59,24 @@ namespace Thomas_Fermi {
 															% (4.0 * r * r * std::exp(- 2.0 * r));
 			}
 
-			ofs.close();
+			ofs_.close();
 		}
 
-		void MakeRhoEnergy::saveresult(std::size_t n, bool useAVXorSSE, bool usecilk)
+        void MakeRhoEnergy::saveresult(std::size_t n, bool usecilk, bool usesimd, double Z)
 		{
-			// 例外指定
-			ofs.exceptions(std::ios::badbit | std::ios::failbit);
+            FEM_ALL::Gauss_Legendre gl(n, usecilk);
 
-			std::cout << boost::format("y'(0) = %.15f\n") % v1_;
+            const double a = 128.0 * Z * Z / (9.0 * power(boost::math::constants::pi<double>(), 2));
+
+            k_ = a / gl.qgauss(
+                xvec_[0],
+                xvec_[size_ - 1],
+                usesimd,
+                [&](double x) { return x * x * rhofunc(x); });
+
 			std::cout << boost::format("Energy = %.15f * Z^(7/3) (Hartree)\n") % makeEnergy();
 
-			ofs.open("Energy.txt");
+			ofs_.open("Energy.txt");
 			
 			ofs << boost::format("y'(0) = %.15f\n") % v1_;
 			ofs << boost::format("Energy = %.15f * Z^(7/3) (Hartree)\n") % makeEnergy();
@@ -91,10 +88,7 @@ namespace Thomas_Fermi {
 			saveresult3();
 		}
 
-#ifdef __GXX_EXPERIMENTAL_CXX0X__
-		constexpr
-#endif
-		double power(double x, std::uint32_t y)
+		constexpr double power(double x, std::uint32_t y)
 		{
 			return y == 0 ? 1 : power(x, y - 1) * x;
 		}
--- Thomas_Fermi/MakeRhoEn/MakeRhoEnergy.h
+++ Thomas_Fermi/MakeRhoEn/MakeRhoEnergy.h
@@ -1,69 +1,156 @@
 ﻿#include "../Beta.h"
-#include <tuple>
-#include <memory>
-#include <fstream>
-#include <utility>
 #include <cstdint>
+#include <fstream>
+#include <memory>
+#include <tuple>
+#include <utility>
 #include <boost/cast.hpp>
-
-#if !defined(__INTEL_COMPILER) && !defined(__GXX_EXPERIMENTAL_CXX0X__) && (_MSC_VER < 1800)
-	#include <boost/noncopyable.hpp>
-#endif
 
 namespace Thomas_Fermi {
 	namespace MakeRhoEn {
-		class MakeRhoEnergy 
-#if !defined(__INTEL_COMPILER) && !defined(__GXX_EXPERIMENTAL_CXX0X__) && (_MSC_VER < 1800)
-			: private boost::noncopyable
-#endif
+		class MakeRhoEnergy final
 		{
-#if defined(__INTEL_COMPILER) || defined(__GXX_EXPERIMENTAL_CXX0X__) || (_MSC_VER >= 1800)
+            typedef std::tuple<std::vector<double>, std::shared_ptr<const FEM_ALL::Beta>,
+                double> parameter_type;
+
+            //! A static const variable.
+            /*!
+            エネルギーを計算するときの定数の値
+            */
+            static const double ALPHA;
+
+            //! A private const variable.
+            /*!
+            x方向のメッシュの刻み幅
+            */
+            const double dx_;
+
+            //! A private variable.
+            /*!
+            規格化のための定数
+            */
+            double k_;
+
+            //! A private const variable.
+            /*!
+            ファイル出力するときのループの最大数
+            */
+            const std::int32_t max_;
+
+            //! A private const variable.
+            /*!
+            Betaクラスのオブジェクトへのスマートポインタ
+            */
+            const std::shared_ptr<const FEM_ALL::Beta> pbeta_;
+
+            //! A private const variable.
+            /*!
+            x方向のメッシュが格納されたstd::vectorのサイズ
+            */
+            const std::size_t size_;
+
+            //! A private const variable.
+            /*!
+            x方向のメッシュ
+            */
+            const std::vector<double> xvec_;
+
+            //! A private variable.
+            /*!
+            ファイル出力用のストリーム
+            */
+            std::ofstream ofs_;
+
+            //! デフォルトコンストラクタ（禁止）
+            /*!
+              デフォルトコンストラクタ（禁止）
+            */
+            MakeRhoEnergy() = delete;
+
+            //! コピーコンストラクタ（禁止）
+            /*!
+              \param コピー元のオブジェクト
+            */
 			MakeRhoEnergy(const MakeRhoEnergy &) = delete;
-			MakeRhoEnergy & operator=(const MakeRhoEnergy &) = delete;
-			MakeRhoEnergy() = delete;
-#endif
-			typedef std::tuple<std::vector<double>, std::shared_ptr<const FEM_ALL::Beta>,
-							   double> parameter_type;
-			static const double ALPHA;
 
-			const std::vector<double> xvec_;
-			const std::shared_ptr<const FEM_ALL::Beta> pbeta_;
-			
-			std::ofstream ofs;
-			
-			const std::size_t size_;
+            //! operator=()（禁止）
+            /*!
+              \param コピー元のオブジェクト（禁止）
+              \return コピー元のオブジェクト
+            */
+            MakeRhoEnergy & operator=(const MakeRhoEnergy &) = delete;
 
-			const double v1_;
-			const double dx_;
+            //! 原子のエネルギーを求める
+            /*!
+            \return 原子のエネルギーを求める
+            */
+            double makeEnergy() const;
 
-			const std::int32_t max_;
+            //! 関数ρ(x)の値を返す
+            /*!
+            \param x xの値
+            \return ρ(x)の値
+            */
+            double rhofunc(double x) const;
 
-			double a;
+            //! 関数y(x)の値をファイルに書き込む
+            /*!
+            \param filename 書き込むファイル名
+            */
+            void savey(const std::string & filename);
 
-			double y(double x) const;
-			double rhofunc(double x) const;
-			double makeEnergy() const;
-			void saveresult1();
-			void saveresult2(std::size_t n, bool useAVXorSSE, bool usecilk);
-			void saveresult3();
+            //! 関数ρ(x)の値をファイルに書き込む
+            /*!
+            \param filename 書き込むファイル名
+            */
+            void saverho(const std::string & filename);
+
+            //! operator=()（禁止）
+            /*!
+            \param コピー元のオブジェクト（禁止）
+            \return コピー元のオブジェクト
+            */
+            void saveresult3();
+
+            //! 関数y(x)の値を返す
+            /*!
+              \param x xの値
+              \return y(x)の値
+            */
+            double y(double x) const;
+
+
+
+
+
+
 
 		public:
 			MakeRhoEnergy::MakeRhoEnergy(const parameter_type & pt);
-			void saveresult(std::size_t n, bool useAVXorSSE, bool usecilk);
+
+            //! 計算結果をファイルに出力する
+            /*!
+            \param n Gauss-Legendreの分点
+            \param usecilk Intel Cilk Plusを使うかどうか
+            \param usesimd SIMDを使うかどうか
+            \param Z 原子番号
+            */
+			void saveresult(std::size_t n, bool usecilk, bool usesimd, double Z);
 		};
 
 		inline MakeRhoEnergy::MakeRhoEnergy(const parameter_type & pt)
 			:	xvec_(std::get<0>(pt)), pbeta_(std::get<1>(pt)),
-				size_(xvec_.size()), v1_(std::get<2>(pt)),
+				size_(xvec_.size()),
 				dx_((xvec_[2] - xvec_[1]) * 2.0),
 				max_(boost::numeric_cast<std::int32_t>(xvec_[size_ - 1] /
 					 MakeRhoEnergy::ALPHA / dx_))
 		{
+            // 例外指定
+            ofs_.exceptions(std::ios::badbit | std::ios::failbit);
+            ofs_.setf(std::ios::fixed, std::ios::floatfield);
+            ofs_ << std::setprecision(15);
 		}
 
-#ifdef __GXX_EXPERIMENTAL_CXX0X__
-		constexpr
-#endif
-		double power(double x, std::uint32_t y);
+		constexpr double power(double x, std::uint32_t y);
 	}
 }
